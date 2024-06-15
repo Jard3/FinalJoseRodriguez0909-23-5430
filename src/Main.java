e7ef7dafb96c87f749fdb20d1cda44ea044bfcc2
José Rodriguez <jard3r5@gmail.com>
2024-06-15 07:25:09 -0600

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+System.out.println("Ingrese 1 si desea ejecutar el primer ejercicio");
+System.out.println("Ingrese 2 si desea ejecutar el segundo ejercicio");
+int opcion = Scanner.nextInt();
+if (opcion == 1) {}
+else if (opcion == 2) {}
+else {System.out.println("Ingrese una opción válida");}
+        }
+    }
